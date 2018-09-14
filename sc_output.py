b1049ae6b62cbc6ad63618506f54e75c25c903b1
Gabriel Moraes <ggmoraes07@gmail.com>
2018-09-14 16:55:12 -0300

    script output

---
+++ sc_output.py
@@ -0,0 +1,28 @@
+import E.py
+import time
+
+SOMA=0.0
+H=getattr(DATA, E.H)
+O='EXTRATO DIA {}\n\n'.format(time.strftime('%d/%m/%y'))
+for x in H:
+	O+='-----------------------------------------------------------'
+	if x['CLIENTE']=='CARGA':
+		O+='\nCARGA\n'
+		for y in ['BRT1','VRT1','BRT2','BRT3','BRDZ','VRDZ','BRMD','VRMD']:
+		if y in x:
+			O+='{}: {}	PREÇO CAIXA: R$ {}	SUBTOTAL: R$ {}\n'.format(y,x[y],x[y+'_DZ'],x[y+'_VLR'])
+		O+='\nTOTAL: R$ {}\n'.format(x['TOTAL'])
+		SOMACARGA=x['TOTAL']
+	else:
+		O+='\n{}\n'.format(x['CLIENTE'])
+		for y in ['BRT1','VRT1','BRT2','BRT3','BRDZ','VRDZ','BRMD','VRMD']:
+			if y in x:
+				O+='{}: {}	PREÇO DÚZIA: R$ {}	SUBTOTAL: R$ {}\n'.format(y,x[y],x[y+'_DZ'],x[y+'_VLR'])
+		O+='\nTOTAL: R$ {}\n'.format(x['TOTAL'])
+		SOMA+=x['TOTAL']
+O+='-----------------------------------------------------------\n'
+O+='TOTAL COMPRADO: R$ {}\nTOTAL VENDIDO: R$ {}\n'.format(SOMACARGA, SOMA)
+O+='LUCRO: R$ {}'.format(SOMA-SOMACARGA)
+P=open('EXTRATO_{}.txt'.format(str.replace(self.ids.SPIN.text, '/', '_')), 'w')
+P.write(O)
+P.close()
