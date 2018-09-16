9b909fec56cab56ccd624029034a8016ec698f8e
Gabriel Moraes <ggmoraes07@gmail.com>
2018-09-16 19:19:12 -0300

    utf8 e rota terça

--- DATA.py
+++ DATA.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ## VALORES FIXOS
 
 _CV={'V1': '0.0', 'V2': '0.0', 'V3': '0.0', 'V4': '0.0', 'V5': '0.0', 'V6': '0.0', 'V7': '0.0', 'V8': '0.0'}
@@ -6,7 +8,7 @@ _V={'V1': '4.0', 'V2': '4.4', 'V3': '3.6', 'V4': '3.2', 'V5': '4.0', 'V6': '4.4'
 
 ## ROTAS
 
-_TERCA=['GIBA', 'LOCATELI', 'SARA', 'TRENZINHO']
+_TERCA=['GIBA', 'LOCATTELI', 'SARA', 'LORI', 'TRENZINHO', 'VERDES CAMPOS', 'ACADEMIA', 'ADEMIR', 'NOVO PAN', 'PASSARINHO', 'JÚLIO', 'ALICE', 'POSTAÇO', 'ESCOLINHA', 'SAPIRANGA', 'MARLI', 'PAULO PILATTI', 'MIRIAM', 'BAUM', 'FEEVALE', 'TAILOR', 'FÁBIO', 'CERRO LARGO', 'DEISE', 'REMI', 'COSTANEIRA', 'GILMAR', 'ASSIS', 'WANDA', 'LILI', 'SCHUTZ', 'SUPER BEM', 'EVANDRO', 'REGINA', 'VÓ', 'NONOAI', 'CLÁUDIO']
 _QUARTA=['DELCIO','SAMUEL']
 _QUINTA=['RICCI','NAOSEI','PADOCA']
 
